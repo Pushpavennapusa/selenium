c318a4457b1686011c745cb85ccd9cbd12658e3d
buddy's <buddy's@buddys-PC>
2018-03-03 12:03:51 +0530

    initial commit

---
+++ src/github_practice/Sample_github.java
@@ -0,0 +1,10 @@
+package github_practice;
+
+public class Sample_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Pushpa Welcome");
+	}
+
+}
